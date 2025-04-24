e6189f1ac62d556f28364008556b8dd1968c4a46
caiyukui <wenqian.cai@tuya.com>
2025-04-24 18:26:19 +0800

    1、数据类 2、doc

--- src/main/java/com/it/po/Book.java
+++ src/main/java/com/it/po/Book.java
@@ -1,60 +1,12 @@
 package com.it.po;
 
+import lombok.Data;
+
+@Data
 public class Book {
     private Integer id;
     private String type;
     private String name;
     private String description;
 
-    public Book() {
-    }
-
-    public Book(Integer id, String type, String name, String description) {
-        this.id = id;
-        this.type = type;
-        this.name = name;
-        this.description = description;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "id=" + id +
-                ", type='" + type + '\'' +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                '}';
-    }
 }
